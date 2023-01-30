5e7e1d6e3c28b1e105bdc050e1e7d44a9172fc2b
Olga Klepikova <olgaklepikova1@yandex.ru>
2023-01-30 17:55:35 +0300

    Изменила отступ

--- src/main/java/app/pages/ProfilePage.java
+++ src/main/java/app/pages/ProfilePage.java
@@ -9,10 +9,8 @@ public class ProfilePage extends BasePage {
     public SelenideElement headerLocator = $x("//h1");
 
     public SelenideElement locatorLogout = $x("//a[@href='/logout/']");
-
     public SelenideElement createAccount = $x("//a[@href ='/register/']");
     public SelenideElement tabReddit = $x("//a[@href='#reddit']");
-
     public SelenideElement checkRedditAccounts = $x("//a[@href='/profile/add/reddit']");
 
 
